51108f536e54b27d8a73de6d04a75ac225a6fa90
Alexander Mishchenko <5740235@gmail.com>
2018-09-09 14:39:36 +0300

    Implement LiveData in the MainViewModel class

--- app/src/main/java/com/rocketbank/rockettest/MainViewModel.kt
+++ app/src/main/java/com/rocketbank/rockettest/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.rocketbank.rockettest
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 /**
@@ -11,15 +12,34 @@ import android.arch.lifecycle.ViewModel
 class MainViewModel : ViewModel() {
 
     private lateinit var repository: Repository
-    val imageA: Image get() = repository.imageA
-    val imageB: Image get() = repository.imageB
+    val imageA: Image? get() = repository.imageA
+    val imageB: Image? get() = repository.imageB
+    val size: Size get() = repository.size
+    val updatesA = MutableLiveData<Pixel>()
+    val updatesB = MutableLiveData<Pixel>()
 
     fun onCreate() {
         repository = RocketApp.objectGraph.get(Repository::class.java)
+        repository.onPixelFilledA = { pixel ->
+            updatesA.postValue(pixel)
+        }
+        repository.onPixelFilledB = { pixel ->
+            updatesB.postValue(pixel)
+        }
     }
 
     fun generateNew() {
         repository.generateImages()
+    }
+
+    fun startFromPixel(pixel: Pixel) {
+        repository.startFromPixel(pixel)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        repository.stopAllHandlers()
     }
 
 }
